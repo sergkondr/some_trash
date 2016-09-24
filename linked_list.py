3332c948bd6b65dfe2132fe1cce02596753b5d49
sergey <kondr.s.a@yandex.ru>
2016-09-24 11:30:27 +1000

    Initial commit My implementation of linked list

---
+++ linked_list.py
@@ -0,0 +1,86 @@
+from random import choice
+
+class Element():
+    def __init__(self, value, prev = None, next = None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return '[ %s ]' % self.value
+
+
+class LinkedList():
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def __str__(self):
+        if self.first != None:
+            if self.first == self.last:
+                return '[ %s ]' % self.first.value
+            current = self.first
+            out_string = '[' + ' %s <->' % str(current.value)
+            while current.next != self.last:
+                current = current.next
+                out_string += ' %s <->' % str(current.value)
+            out_string += ' %s ]' % self.last.value
+            return out_string
+        else:
+            return 'The list is empty'
+
+    def random(self, minimal=3, maximal=10):
+        for i in range(choice(range(minimal, maximal))):
+            self.add(choice(range(10)))
+        print('Generated random linked list:\n', self)
+
+    def add(self, value, index=None):
+        self.length += 1
+        if self.first != None and index == None:
+            self.last.next = self.last = Element(value, self.last)
+        elif self.first == None and index == None:
+            self.first = self.last = Element(value, self.last)
+        elif index >= self.length - 1 or index < 0:
+            self.length -= 1
+            print('Index must be in range 0 to %s' % str(self.length - 1))
+        else:
+            current = self.first
+            for i in range(index + 1):
+                if i == index:
+                    if current == self.first:
+                        self.first = self.first.prev = Element(value, None, self.first)
+                    elif current == self.last:
+                        self.last.next = self.last = Element(value, self.last)
+                    else:
+                        current.prev.next = current.prev = Element(value, current.prev, current)
+                    break
+                current = current.next
+
+    def delete(self, index):
+        if index >= self.length or index < 0:
+            print('Index must be in range 0 to %s' % str(self.length - 1))
+        else:
+            current = self.first
+            for i in range(index + 1):
+                if i == index:
+                    if current == self.first:
+                        self.first = self.first.next
+                        self.first.prev = None
+                    elif current == self.last:
+                        self.last = self.last.prev
+                        self.last.next = None
+                    else:
+                        current.prev.next = current.next
+                        current.next.prev = current.prev
+                    break
+                current = current.next
+
+    def clear(self):
+        self.__init__()
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.random()
+    for i in ll:
+        print(i.value)
