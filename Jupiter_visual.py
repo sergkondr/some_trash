e1d7a34a2e3399c6ee36ed4bbafea597aaf70dc7
Serg Kondrashov <kondr.s.a@yandex.ru>
2016-11-06 23:00:57 +1000

    Update Jupiter_visual.py

--- Jupiter_visual.py
+++ Jupiter_visual.py
@@ -14,16 +14,28 @@ class Node():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        
         self.title = ''.join(choice(self.ABC) for x in range(4))
+        self.links = []
+
+    def __str__(self):
+        return '<%s>' % self.title
+
+    def __repr__(self):
+        return self.__str__()
+
+    def add_link(self, neighbor, weight):
+        self.links.append({neighbor: weight})
+
 
 def in_cluster(node1, node2):
     distance = sqrt(pow((node1.x - node2.x), 2) + pow((node1.y - node2.y), 2))
     if distance < cluster_radius:
-        return True
+        return int(distance)
     else:
-        return False
+        return -1
         
-def generate_nodes(num=25):
+def generate_nodes(num=20):
     nodes = []
     for number_of_nodes in range(num):
         x = choice(range(100))
@@ -33,18 +45,21 @@ def generate_nodes(num=25):
 
 def draw_nodes(nodes):
     for node in nodes:
-        plt.plot(node.x, node.y, 'rx')
+        plt.plot(node.x, node.y, 'ro')
         plt.annotate(node.title, xy=(node.x, node.y), xytext=(node.x, node.y))
 
 def get_links(nodes):        
     for node1 in nodes:
         for node2 in nodes:
-            if node1 != node2:
-                if in_cluster(node1, node2):
-                    plt.plot([node1.x, node2.x], [node1.y, node2.y]) 
+            if node1 != node2 and in_cluster(node1, node2) > 0:
+                plt.plot([node1.x, node2.x], [node1.y, node2.y])
+                node1.add_link(node2, in_cluster(node1, node2))
                     
 nodes = generate_nodes()
 draw_nodes(nodes)
 get_links(nodes)
+
+for node in nodes:
+    print('%s: %s' %(node, node.links))
 
 plt.show()
